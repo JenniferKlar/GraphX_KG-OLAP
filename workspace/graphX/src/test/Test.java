d9f792e828cffdb87f0c5c84cef2bb57a4ddf426
JenniferKlar <jennyklar@gmx.at>
2022-02-20 06:52:06 +0100

    cleaned up repository

--- workspace/graphX/src/test/Test.java
+++ workspace/graphX/src/test/Test.java
@@ -2,10 +2,10 @@ package test;
 
 import org.junit.After;
 import org.junit.Before;
+
 import org.junit.Assert;
 
-import java.time.LocalDateTime;
-import java.util.List;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import org.apache.spark.SparkConf;
@@ -13,9 +13,7 @@ import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.graphx.Graph;
 
 import graphX.GraphGenerator;
-import graphX.GraphGenerator2;
 import graphX.NQuadReader;
-import graphX.NQuadReader2;
 import graphX.Relation;
 import scala.reflect.ClassTag;
 
@@ -29,18 +27,27 @@ public class Test {
 			.set("spark.executor.memory", "2g").set("spark.executor.cores", "1")
 			.set("spark.dynamicAllocation.enabled", "true")
 			.set("spark.serializer", "org.apache.spark.serializer.JavaSerializer")
-			//.set("spark.eventLog.enabled", "true")
+			.set("spark.eventLog.enabled", "true")
+			.set("spark.eventLog.dir", "C:\\Users\\jenniffer\\Desktop")
 			;
 	JavaSparkContext jsc = new JavaSparkContext(sparkConf);
+	
 
 	@Before
 	public void setLogLevel() {
 		this.jsc.setLogLevel("ERROR");
+		
 	}
 
 	@After
 	public void shutDownJSC() {
-		jsc.close();
+		try {
+			System.in.read();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		jsc.stop();
 	}
 
 //	@org.junit.Test
@@ -109,33 +116,33 @@ public class Test {
 //		Assert.assertEquals(7, count1);
 //		Assert.assertEquals(7, count2);
 //	}
+////	
+//	@org.junit.Test
+//	public void testPivot() {
+//		String fileName = "pivot.nq";
+//		Graph<Object, Relation> graph = GraphGenerator2.generateGraph(jsc, objectTag, relationTag, path, fileName);
+//		
+//		ArrayList<String> context = new ArrayList<String>();
+//		context.add("urn:uuid:2c95e204-26ea-43ec-a997-774b5dc41c6d-mod");
+//		context.add("urn:uuid:2c95e204-26ea-43ec-a997-774b5dc41c6test-mod");
+//		
+//		Graph<Object, Relation> pivotGraph = NQuadReader2.pivot(graph, jsc, objectTag, relationTag, "hasLocation",
+//		"http://example.org/kgolap/object-model#hasLocation",
+//		"http://example.org/kgolap/object-model#ManoeuvringAreaAvailability",
+//		context);
+//		pivotGraph.triplets().toJavaRDD().foreach(x -> System.out.println(x.srcAttr() + " " + x.attr().getRelationship() + " " + x.dstAttr() + " " + x.attr().getContext()));
+//
+//		
+//		long count1 = graph.edges().toJavaRDD().filter(x -> x.attr().getRelationship().toString().contains("hasLocation")).count();
+//		long count2 = pivotGraph.edges().toJavaRDD().filter(x -> x.attr().getRelationship().toString().contains("hasLocation")).count();
+//		long count3 = pivotGraph.edges().toJavaRDD().filter(x -> x.attr().getRelationship().toString().contains("http://example.org/kgolap/object-model#hasLocation")).count();
+//		Assert.assertNotEquals(count1, count2);
+//		Assert.assertEquals(1, count1);
+//		Assert.assertEquals(3, count2);
+//		Assert.assertEquals(2, count3);
+//		
+//	}	
 //	
-	@org.junit.Test
-	public void testPivot() {
-		String fileName = "pivot.nq";
-		Graph<Object, Relation> graph = GraphGenerator2.generateGraph(jsc, objectTag, relationTag, path, fileName);
-		
-		ArrayList<String> context = new ArrayList<String>();
-		context.add("urn:uuid:2c95e204-26ea-43ec-a997-774b5dc41c6d-mod");
-		context.add("urn:uuid:2c95e204-26ea-43ec-a997-774b5dc41c6test-mod");
-		
-		Graph<Object, Relation> pivotGraph = NQuadReader2.pivot(graph, jsc, objectTag, relationTag, "hasLocation",
-		"http://example.org/kgolap/object-model#hasLocation",
-		"http://example.org/kgolap/object-model#ManoeuvringAreaAvailability",
-		context);
-		pivotGraph.triplets().toJavaRDD().foreach(x -> System.out.println(x.srcAttr() + " " + x.attr().getRelationship() + " " + x.dstAttr() + " " + x.attr().getContext()));
-
-		
-		long count1 = graph.edges().toJavaRDD().filter(x -> x.attr().getRelationship().toString().contains("hasLocation")).count();
-		long count2 = pivotGraph.edges().toJavaRDD().filter(x -> x.attr().getRelationship().toString().contains("hasLocation")).count();
-		long count3 = pivotGraph.edges().toJavaRDD().filter(x -> x.attr().getRelationship().toString().contains("http://example.org/kgolap/object-model#hasLocation")).count();
-		Assert.assertNotEquals(count1, count2);
-		Assert.assertEquals(1, count1);
-		Assert.assertEquals(3, count2);
-		Assert.assertEquals(2, count3);
-		
-	}	
-	
 //	
 //	@org.junit.Test
 //	public void testReification() {
@@ -145,11 +152,10 @@ public class Test {
 //		contexts.add("urn:uuid:0acc4b38-168d-4a33-898c-258b89881556-mod");
 //		Graph<Object, Relation> reifiedGraph = 
 //		NQuadReader2.reify(graph, jsc, objectTag, relationTag,
-//				contexts, 
 //				"http://example.org/kgolap/object-model#usage",
 //				"http://example.org/kgolap/object-model#usage-type", 
 //				"http://www.w3.org/1999/02/22-rdf-syntax-ns#object", 
-//				"http://www.w3.org/1999/02/22-rdf-syntax-ns#subject");
+//				"http://www.w3.org/1999/02/22-rdf-syntax-ns#subject", contexts);
 //
 //		long count1 = graph.triplets().toJavaRDD().count();
 //		long count2 = reifiedGraph.triplets().toJavaRDD().count();
@@ -161,29 +167,29 @@ public class Test {
 //		reifiedGraph.triplets().toJavaRDD().foreach(x -> System.out.println(x.srcAttr() + " " + x.attr().getRelationship() + " " + x.dstAttr() + " " + x.attr().getContext()));
 //	}
 	
-//	@org.junit.Test
-//	public void testGroupByProperty() {
-//		String fileName = "groupByProperty.nq";
-//		Graph<Object, Relation> graph = GraphGenerator2.generateGraph(jsc, objectTag, relationTag, path, fileName);
-//
-//		ArrayList<String> contexts = new ArrayList<String>();
-//		contexts.add("urn:uuid:8378d3c2-575d-4cb8-874b-eb4ae286d61b-mod");
-//		Graph<Object, Relation> resultGraph = NQuadReader2.groupByProperty(graph, jsc, objectTag, relationTag,
-//				"operationalStatus", "http://example.org/kgolap/object-model#grouping",contexts
-//				);
-//
-//		long groupingCount = resultGraph.triplets().toJavaRDD()
-//				.filter(x -> x.attr().getRelationship().toString().contains(
-//						"http://example.org/kgolap/object-model#grouping") && x.dstAttr().toString().contains("-Group"))
-//				.count();
-//
-//		Assert.assertNotEquals(resultGraph, graph);
-//		Assert.assertEquals(6, groupingCount);
-//
-//		long groupCountDistinct = resultGraph.triplets().toJavaRDD()
-//				.filter(x -> x.srcAttr().toString().contains("-Group")).map(x -> x.srcId()).distinct().count();
-//		Assert.assertEquals(3, groupCountDistinct);
-//	}
+	@org.junit.Test
+	public void testGroupByProperty() {
+		String fileName = "groupByProperty.nq";
+		Graph<Object, Relation> graph = GraphGenerator.generateGraph(jsc, objectTag, relationTag, path, fileName);
+
+		ArrayList<String> contexts = new ArrayList<String>();
+		contexts.add("urn:uuid:8378d3c2-575d-4cb8-874b-eb4ae286d61b-mod");
+		Graph<Object, Relation> resultGraph = NQuadReader.groupByProperty(graph, jsc, objectTag, relationTag,
+				"operationalStatus", "http://example.org/kgolap/object-model#grouping",contexts
+				);
+
+		long groupingCount = resultGraph.triplets().toJavaRDD()
+				.filter(x -> x.attr().getRelationship().toString().contains(
+						"http://example.org/kgolap/object-model#grouping") && x.dstAttr().toString().contains("-Group"))
+				.count();
+
+		Assert.assertNotEquals(resultGraph, graph);
+		Assert.assertEquals(6, groupingCount);
+
+		long groupCountDistinct = resultGraph.triplets().toJavaRDD()
+				.filter(x -> x.srcAttr().toString().contains("-Group")).map(x -> x.srcId()).distinct().count();
+		Assert.assertEquals(3, groupCountDistinct);
+	}
 
 //	@org.junit.Test
 //	public void testReplaceByGrouping() {
@@ -221,7 +227,7 @@ public class Test {
 // 
 //		
 //		Graph<Object, Relation> wingspanSum = NQuadReader2.aggregatePropertyValues(replacedGraph, jsc, objectTag, relationTag, 
-//				 "http://example.org/kgolap/object-model#wingspan", contexts, "SUM"); 
+//				 "http://example.org/kgolap/object-model#wingspan","SUM", contexts); 
 //		 
 ////		wingspanSum.triplets().toJavaRDD().foreach
 ////		(x -> System.out.println(x.srcAttr() + " " + x.attr().getRelationship() + " " + x.dstAttr()+ " " + x.attr().getContext()));
@@ -231,7 +237,7 @@ public class Test {
 //		Assert.assertEquals(1, sum1);
 //		Assert.assertEquals(1, sum2);
 //	}
-	
+//	
 //	@org.junit.Test
 //	public void testAggregateByPropertyGroupingCOUNT() {
 //		String fileName = "aggregateByProperty.nq";
@@ -246,14 +252,14 @@ public class Test {
 //		System.out.println("----------------");
 //		
 //		Graph<Object, Relation> wingspanSum = NQuadReader2.aggregatePropertyValues(replacedGraph, jsc, objectTag, relationTag, 
-//				 "http://example.org/kgolap/object-model#wingspan", contexts, "COUNT"); 
+//				 "http://example.org/kgolap/object-model#wingspan", "COUNT",contexts); 
 //		 wingspanSum.triplets().toJavaRDD().foreach
 //			(x -> System.out.println(x.srcAttr() + " " + x.attr().getRelationship() + " " + x.dstAttr()+ " " + x.attr().getContext()));
 //		long count = wingspanSum.triplets().toJavaRDD().filter(x -> x.dstAttr().toString().equals("2")).count();
 //		
 //		Assert.assertEquals(3, count);
 //	}
-	
+//	
 //	@org.junit.Test
 //	public void testAggregateByPropertyGroupBySUM() {
 //		String fileName = "aggregateByProperty.nq";
@@ -264,7 +270,7 @@ public class Test {
 //		Graph<Object, Relation> replacedGraph 
 //		 = NQuadReader2.groupByProperty(graph, jsc, objectTag, relationTag,"http://example.org/kgolap/object-model#wingspanInterpretation", "http://example.org/kgolap/object-model#grouping", contexts);
 //		 Graph<Object, Relation> resultGraph = NQuadReader2.aggregatePropertyValues(replacedGraph, jsc, objectTag, relationTag, 
-//		"http://example.org/kgolap/object-model#wingspan", contexts, "SUM"); 
+//		"http://example.org/kgolap/object-model#wingspan", "SUM", contexts); 
 //		 
 //			long sum1 = resultGraph.triplets().toJavaRDD().filter(x -> x.dstAttr().toString().contains("3061074470")).count();
 //			long sum2 = resultGraph.triplets().toJavaRDD().filter(x -> x.dstAttr().toString().contains("1227604772")).count();
@@ -272,7 +278,7 @@ public class Test {
 //			Assert.assertEquals(1, sum1);
 //			Assert.assertEquals(1, sum2);
 //	}
-	
+//	
 //	@org.junit.Test
 //	public void testAggregateByPropertyGroupByCOUNT() {
 //		String fileName = "aggregateByProperty.nq";
@@ -284,7 +290,7 @@ public class Test {
 //		 = NQuadReader2.groupByProperty(graph, jsc, objectTag, relationTag,"http://example.org/kgolap/object-model#wingspanInterpretation", "http://example.org/kgolap/object-model#grouping", 
 //				 contexts);
 //		 Graph<Object, Relation> resultGraph = NQuadReader2.aggregatePropertyValues(replacedGraph, jsc, objectTag, relationTag, 
-//		"http://example.org/kgolap/object-model#wingspan", contexts, "COUNT"); 
+//		"http://example.org/kgolap/object-model#wingspan",  "COUNT", contexts); 
 //		 
 //			long count = resultGraph.triplets().toJavaRDD().filter(x -> x.dstAttr().toString().equals("2")).count();
 //			Assert.assertEquals(3, count);
